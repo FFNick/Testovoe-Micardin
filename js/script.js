d4e3f2e05bba49d634f86e9ff9355fec3a9c84f5
FF_Nick <dmitrij.petrukh@yandex.ru>
2023-01-24 21:20:03 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
     $('.slider').slick({
       arrows: false,
       dots: true,
-      
+
     });
   });
+
+  $(document).ready(function(){
+    $('.slider1').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+
+    });
+  });
+
+  
